ce1f40d91f8cac1648b7c617aad304922c633ad6
garcesvillareal <Garcesvillareal@hotmail.com>
2022-02-15 13:59:43 -0500

    Edutka v4.0.7
    Actualización final.

--- includes/info.php
+++ includes/info.php
@@ -1,8 +1,8 @@
 <?php
-$mysql_hostname = "localhost";
-$mysql_username = "root";
-$mysql_password = "";
-$mysql_database = "edutka";
+$mysql_hostname = "localhost"; // Nombre del servidor
+$mysql_username = "root"; // Usuario de acceso a la base de datos
+$mysql_password = ""; // Contraseña de acceso a la base de datos
+$mysql_database = "edutka"; // Nombre de la base de datos
 
-$site_url = "http://192.168.1.52/cenauc"; // e.g (http://example.com)
+$site_url = "http://192.168.1.52/cenauc"; // Url del sitio web, ej. (http://example.com)
 ?>
